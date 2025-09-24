90167c4f61990a738cd13c5e03d66f477a994592
Isaac.Lera <isaac.lera@gmail.com>
2025-09-24 13:48:46 +0200

    updates

--- U2-RPC/B_restful-app.py
+++ U2-RPC/B_restful-app.py
@@ -4,27 +4,40 @@ from flask import Flask, request, jsonify
 import random
 app = Flask(__name__)
 
-things = [ ]
+things = {}
+things[1] = {"id": 1, "name": "Thing Value %f"%random.random()}
+things[2] = {"id": 2, "name": "Thing Value %f"%random.random()}
+things[3] = {"id": 3, "name": "Thing Value %f"%random.random()}
 
-@app.get("/random")
+@app.route("/api/v1/random", methods=["GET"])
 def get_random_number():
-    return jsonify({"random":random.random()})
+    return jsonify({"random":random.random()}), 200
 
-@app.get("/things")
-def get_things():
-    return jsonify(things)
+@app.route("/api/v1/things/<int:thing_id>", methods=["GET"])
+def get_things(thing_id):
+    if thing_id in things:
+        return jsonify(things[thing_id]), 200
+    else:
+        return jsonify({"error": "Thing not found"}), 404
 
-@app.post("/addThing")
+@app.route("/api/v1/things", methods=["POST"])
 def add_thing():
     if request.is_json:
-        thing = request.get_json()
-        things.append(thing)
-        return thing, 201
+        data = request.get_json()
+        things[int(data["id"])] = data
+        return jsonify(data), 201
     return {"error": "Request must be JSON"}, 415
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
-# Save the file as: app.py  #or: export FLASK_APP=app.py
-# Run: python -m flask run
-# With curl or browser: http://127.0.0.1:5000/random    
-# curl -i http://127.0.0.1:5000/addThing -X POST -H 'Content-Type: application/json' -d '{"seed":2022}'
+
+# Run: python3 app.py (me: /Users/isaac/.pyenv/versions/3.11.0rc2/envs/my3110/bin/python3 app.py)
+# With curl or browser: http://127.0.0.1:5000/api/v1/random    
+# curl -i http://127.0.0.1:5000/api/v1/things -X POST -H 'Content-Type: application/json' -d '{"id":4, "name":"Thing Value 4.3"}'
+# openapi.yaml in https://editor.swagger.io/
+
+# Guidelines:
+# https://learn.microsoft.com/en-us/azure/architecture/best-practices/api-design
+#https://swagger.io/specification/
---
+++ U2-RPC/C_restful-Swagger.py
@@ -0,0 +1,108 @@
+#Based on: https://realpython.com/api-integration-in-python/
+
+from flask import Flask, request, jsonify
+import random
+from flasgger import Swagger
+
+app = Flask(__name__)
+swagger = Swagger(app)
+things = {}
+things[1] = {"id": 1, "name": "Thing Value %f"%random.random()}
+things[2] = {"id": 2, "name": "Thing Value %f"%random.random()}
+things[3] = {"id": 3, "name": "Thing Value %f"%random.random()}
+
+@app.route("/api/v1/random", methods=["GET"])
+def get_random_number():
+    """
+    Obtener un número aleatorio
+    ---
+    tags:
+      - Random
+    responses:
+      200:
+        description: Número aleatorio
+        content:
+          application/json:
+            schema:
+              type: array
+              items:
+                $ref: '#/components/schemas/Random'
+    """
+    return jsonify({"random":random.random()}), 200
+
+@app.route("/api/v1/things/<int:thing_id>", methods=["GET"])
+def get_things(thing_id):
+    """
+    Obtener un thing por ID
+    ---
+    tags:
+      - Things
+    responses:
+      200:
+        description: Thing encontrado
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/Thing'
+      404:
+        description: Thing no encontrado
+    """
+    if thing_id in things:
+        return jsonify(things[thing_id]), 200
+    else:
+        return jsonify({"error": "Thing not found"}), 404
+
+@app.route("/api/v1/things", methods=["POST"])
+def add_thing():
+    """
+    Crear un nuevo thing
+    ---
+    tags:
+      - Things
+    responses:
+      201:
+        description: Thing creado
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/Thing'
+      415:
+        description: Request must be JSON
+    """
+    if request.is_json:
+        data = request.get_json()
+        things[int(data["id"])] = data
+        return jsonify(data), 201
+    return {"error": "Request must be JSON"}, 415
+
+
+app.config["SWAGGER"] = {
+    "components": {
+        "schemas": {
+            "Random": {
+                "type": "object",
+                "properties": {
+                    "random": {"type": "number"},
+                }
+            },
+            "Thing": {
+                "type": "object",
+                "required": ["id", "name"],
+                "properties": {
+                    "id": {"type": "integer"},
+                    "name": {"type": "string"}
+                }
+            }
+        }
+    }
+}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# Run: python3 app.py (me: /Users/isaac/.pyenv/versions/3.11.0rc2/envs/my3110/bin/python3 app.py)
+# With curl or browser: http://127.0.0.1:5000/api/v1/random    
+# curl -i http://127.0.0.1:5000/api/v1/things -X POST -H 'Content-Type: application/json' -d '{"id":4, "name":"Thing Value 4.3"}'
+# openapi.yaml in https://editor.swagger.io/
